6ae8e38117718d2390667cf7802ee08375471b33
Souheil CHELFOUH <trollfot@vittra.org>
2009-10-21 20:37:42 +0200

    Bumped version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join
 
 name = 'dolmen.thumbnailer'
-version = '0.1'
+version = '0.2dev'
 readme = open(join('src', 'dolmen', 'thumbnailer', 'README.txt')).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
